7dbd8701b52101ae28f8823e9d68614aa1baed49
Christos Kalivas <92673248+areudev@users.noreply.github.com>
2024-04-06 22:47:43 +0300

    Add subscriptions to createSignal function in reactive.ts

--- play/reactive.ts
+++ play/reactive.ts
@@ -1,8 +1,18 @@
+const context: Array<() => void> = []
+
 export function createSignal<T>(
 	initialValue: T
 ): [() => T, (newValue: T) => void] {
+	let subscriptions = new Set<() => void>()
+
 	let value: T = initialValue
-	const read = (): T => value
+
+	const read = (): T => {
+		const observer = context[context.length - 1]
+		if (observer) subscriptions.add(observer)
+
+		return value
+	}
 	const write = (newValue: T): void => {
 		value = newValue
 	}
