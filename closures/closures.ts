86fe35e9e709311e384b4222636c1bb990066308
Christos Kalivas <92673248+areudev@users.noreply.github.com>
2024-04-12 18:47:21 +0300

    Refactor useState function in closures.ts to return a getter function for the state

--- closures/closures.ts
+++ closures/closures.ts
@@ -1,15 +1,15 @@
-export function useState<T>(initialState: T): [T, (newState: T) => void] {
+export function useState<T>(initialState: T): [() => T, (newState: T) => void] {
 	let state = initialState
 
 	const setState = (newState: T) => {
 		state = newState
 	}
 
-	return [state, setState]
+	return [() => state, setState]
 }
 
 const [count, setCount] = useState(0)
 
-console.log(count)
+console.log(count())
 setCount(10)
-console.log(count)
+console.log(count())
